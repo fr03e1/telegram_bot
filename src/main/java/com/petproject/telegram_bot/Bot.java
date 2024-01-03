f36936330ff2a4164b8da2adb03c8fe15d3e14f9
a.bocharov <a.bocharov@msocialproduction.com>
2024-01-03 04:07:28 +0300

    add tg bot lib

---
+++ src/main/java/com/petproject/telegram_bot/Bot.java
@@ -0,0 +1,27 @@
+package com.petproject.telegram_bot;
+
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+@Component
+@Slf4j
+@Getter
+public class Bot extends TelegramLongPollingBot {
+    public static long BOT_ID = -1;
+    @Override
+    public String getBotUsername() {
+        return null;
+    }
+
+    @Override
+    public String getBotToken() {
+        return null;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+
+    }
+}
---
+++ src/main/java/com/petproject/telegram_bot/config/SpringConfig.java
@@ -0,0 +1,20 @@
+package com.petproject.telegram_bot.config;
+
+import com.petproject.telegram_bot.Bot;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+@Configuration
+public class SpringConfig {
+
+    @Bean
+    public TelegramBotsApi telegramBotsApi(Bot bot) throws TelegramApiException {
+        final TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+        telegramBotsApi.registerBot(bot);
+        Bot.BOT_ID = bot.getMe().getId();
+        return telegramBotsApi;
+    }
+}
